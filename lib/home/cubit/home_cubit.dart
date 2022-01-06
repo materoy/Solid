33a5c1e62916b1501d966504279e32cd17895bcc
Roy Matero <roymatero@gmail.com>
2022-01-06 17:38:37 +0300

    implement base features

--- lib/home/cubit/home_cubit.dart
+++ lib/home/cubit/home_cubit.dart
@@ -9,16 +9,20 @@ class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(const HomeState(0xFFFFFFFF));
   final rand = Random();
 
+  /* Generates a set of 6 random hexadecimal values
+    with 1677216 different possibilites 
+    16 ^ 6 = 1677216 
+    */
   void generateRandomColor() {
     final stringBuffer = StringBuffer('0xFF');
 
     for (var i = 0; i < 6; i++) {
-      final value = rand.nextInt(255).toRadixString(16);
-      stringBuffer.write(value);
+      final value = rand.nextInt(256);
+      final hexValue = value.toRadixString(16);
+      stringBuffer.write(hexValue);
     }
 
     final color = int.parse(stringBuffer.toString());
-    print(color);
     emit(HomeState(color));
   }
 }
--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:solid_software/home/cubit/home_cubit.dart';
@@ -20,15 +23,102 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<HomeCubit, HomeState>(
-      builder: (context, state) {
-        return GestureDetector(
-          onTap: () => context.read<HomeCubit>().generateRandomColor(),
-          child: Scaffold(
-              backgroundColor: Color(state.backgroundColor),
-              body: Center(child: Text(context.l10n.displayText))),
-        );
+    var touchPosition = Offset.zero;
+    return GestureDetector(
+      onTapUp: (details) {
+        touchPosition = details.globalPosition;
+        context.read<HomeCubit>().generateRandomColor();
       },
+      child: Scaffold(
+        body: Stack(
+          children: [
+            SizedBox(
+              height: double.infinity,
+              width: double.infinity,
+              child: BlocBuilder<HomeCubit, HomeState>(
+                builder: (context, state) {
+                  return _AnimatedColorChange(
+                    backgroundColor: Color(state.backgroundColor),
+                    location: touchPosition,
+                  );
+                },
+              ),
+            ),
+            Center(
+              child: AnimatedTextKit(
+                animatedTexts: [
+                  TyperAnimatedText(
+                    context.l10n.displayText,
+                    textStyle: const TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    speed: const Duration(milliseconds: 400),
+                  ),
+                  ColorizeAnimatedText(
+                    context.l10n.displayText,
+                    textStyle: const TextStyle(
+                        fontSize: 26, fontWeight: FontWeight.w800),
+                    colors: [Colors.black, Colors.green],
+                  ),
+                ],
+                totalRepeatCount: 5,
+                pause: const Duration(milliseconds: 2000),
+                displayFullTextOnTap: true,
+                stopPauseOnTap: true,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _AnimatedColorChange extends StatefulWidget {
+  const _AnimatedColorChange({
+    Key? key,
+    required this.backgroundColor,
+    required this.location,
+  }) : super(key: key);
+  final Color backgroundColor;
+  final Offset location;
+
+  @override
+  _AnimatedColorChangeState createState() => _AnimatedColorChangeState();
+}
+
+class _AnimatedColorChangeState extends State<_AnimatedColorChange>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 600),
+    );
+
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.bounceOut);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      // alignment: Alignment(widget.location.dx, widget.location.dy),
+      children: [
+        Transform.scale(
+          scale: 1,
+          child: AnimatedContainer(
+            height: 1000,
+            width: 1000,
+            decoration: BoxDecoration(color: widget.backgroundColor),
+            duration: const Duration(milliseconds: 600),
+          ),
+        ),
+      ],
     );
   }
 }
